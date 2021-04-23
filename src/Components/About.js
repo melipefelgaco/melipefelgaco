a4c000f71cfe033a12755ee8c24f7c2a45710512
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-04-23 13:54:05 -0300

    typo

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,11 +7,10 @@ const About = () => {
       <p>
         Recently I've been coding everyday, and recently started to drink tea
         and liked it. I also relax playing a game or listening to music. Or
-        both. Reading or watching content or talking to other people about
-        coding and technology in general is what keeps me excited to go back to
-        my computer and do some more coding, sometimes just mess with old
-        projects. I don't know what else to say so just ask if you want to learn
-        more.
+        both. Reading, watching content or talking to other people about coding
+        and technology in general is what keeps me excited to go back to my
+        computer and do some more coding, sometimes just mess with old projects.
+        I don't know what else to say so just ask if you want to learn more.
       </p>
     </div>
   );
