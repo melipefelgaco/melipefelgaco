67c6578cec4b489a86026e9d8409e41ee15ae2d8
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-04-30 13:10:00 -0300

    typos and theme button changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,12 +5,13 @@ const About = () => {
     <div className="about" id="about">
       <h1>About me</h1>
       <p>
-        Recently I've been coding everyday, and recently started to drink tea
-        and liked it. I also relax playing a game or listening to music. Or
-        both. Reading, watching content or talking to other people about coding
-        and technology in general is what keeps me excited to go back to my
-        computer and do some more coding, sometimes just mess with old projects.
-        I don't know what else to say so just ask if you want to learn more.
+        Recently I've been coding every day, and started to drink tea and liked
+        it. I unwind playing a game or listening to music. Or both. Reading,
+        watching content or talking to other people about coding and technology
+        in general is what keeps me excited to go back to my computer and do
+        some more coding, sometimes just mess with old projects or solve some
+        logic exercises in order to not get rusty. I don't know what else to say
+        so just ask if you want to learn more.
       </p>
     </div>
   );
