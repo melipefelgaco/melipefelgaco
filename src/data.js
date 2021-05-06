cc858f1fd445c413a8868261d6cf23b26d962b9c
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-05-06 16:22:11 -0300

    data is now its own file and ProjectCard component is optimized

---
+++ src/data.js
@@ -0,0 +1,29 @@
+import cConverterImg from "./images/cConverter.png";
+import starWarsImg from "./images/starWarsApi.png";
+import tarkovImg from "./images/tarkovBallistics.png";
+
+const data = [
+  {
+    img: cConverterImg,
+    title: "Currency Converter",
+    text:
+      "Currency Converter was a project that initially started with vanilla JS and CSS using Bootstrap. Only later it was remade in React. The calculation when it was first deployed used to be based on an API that later changed to a model that required the user to pay for a key to exchange the currency. So I made the change to what is now the current working API.The current API happens to sometimes go down but hey, at least it's free.It was a very fun project to make, and it was my first deployed project. Currently it converts between the following currencies: BRL, USD and EUR.",
+    website: "https://fmagesty.github.io/currency-converter/",
+  },
+  {
+    img: starWarsImg,
+    title: "Search Ships using the Star Wars API",
+    text:
+      "This project is all about working with an API and Json data. It was a project developed by the PodCodar community and asked the developer to search the starwar ships on the API and show it's information without using any library that worked with it. This was a good challenge and interesting project. It was made in React.",
+    website: "https://fmagesty.github.io/react-sw-api/",
+  },
+  {
+    img: tarkovImg,
+    title: "Escape From Tarkov: Ballistics Simulator",
+    text:
+      " EFT: Ballistics Simulator was made for players to solve the problem of not knowing about which of the ingame ammo to use. Until the developers of EFT release a public API this one will be my only EFT project for the moment.",
+    website: "https://fmagesty.github.io/react-tarkov-ballistics/",
+  },
+];
+
+export default data;
